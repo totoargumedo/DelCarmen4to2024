047ccc5ee31bb098b47777044a6fa06215b3b250
totoargumedo <totoargumedo@gmail.com>
2024-09-24 13:26:20 -0300

    intro a JS

---
+++ Clase12-IntroJS/js/script.js
@@ -0,0 +1,29 @@
+// alert("Hola Vieja soy un cartel");
+
+// console.log("Hola vieja soy una consola");
+// console.warn("Hola vieja soy una advertencia");
+// console.error("Hola vieja soy un error");
+
+// Variables
+// var saludo = "hola!"
+// let saludo = "Hola!";
+// const saludo2 = "Holita!";
+
+// alert(saludo);
+// alert(saludo2);
+
+// saludo2 = "Otro hola";
+// alert(saludo2);
+
+//prompts
+
+// console.log(prompt("Nombre"));
+// let edad = prompt("Ingrese su edad");
+// edad = Number(edad);
+// console.log(typeof edad);
+// console.log("Tenes " + edad + " años");
+
+//strings
+let nombre = prompt("Ingrese su nombre");
+nombre = nombre.toUpperCase();
+console.log("Hola " + nombre);
